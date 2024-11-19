c22387131e678c1d9678597967b3f44d1508d3c7
Evan Henry <evan@perfx.cloud>
2024-11-19 16:00:14 -0500

    Removed explicit Makefile for cargo make. The default works just fine for now. Added preliminary parsing of CLI arguments via clap, limited for now just to the mode, the port number, and the logging level via the debug flag. iperf3 is not strict with the value provided for log level, but we are.

---
+++ riperf3-cli/src/cli.rs
@@ -0,0 +1,41 @@
+use clap::{ArgGroup, Parser};
+
+#[derive(Parser, Debug)]
+#[command(about, author, long_about = None, version)]
+#[command(group(
+        ArgGroup::new("mode")
+            .required(true)
+            .args(&["server", "client"])
+))]
+
+// The main CLI struct for the riperf3-cli application.
+pub struct Cli {
+    /// Run in server mode
+    #[arg(short, long, group = "mode")]
+    pub server: bool,
+
+    /// Run in client mode
+    #[arg(short, long, group = "mode", value_name = "HOST")]
+    pub client: Option<String>,
+
+    /// Port number to connect to or listen on
+    #[arg(
+        short,
+        long,
+        default_value_t = 5201,
+        help = "server port to listen on/connect to"
+    )]
+    pub port: u16,
+
+    /// Emit debugging output (optional "=" and debug level: 1-4. Default is 4 - all messages)
+    #[arg(
+        short,
+        long,
+        value_name = "LEVEL",
+        num_args = 0..=1,
+        value_parser = clap::value_parser!(u8).range(1..=4),
+        default_missing_value = "4",
+        help = "emit debugging output (optional '=' and debug level: 1-4. Default is 4 - all messages)"
+    )]
+    pub debug: Option<u8>,
+}
--- riperf3-cli/src/main.rs
+++ riperf3-cli/src/main.rs
@@ -1,11 +1,61 @@
-extern crate riperf3;
+// The main entry point for the riperf3-cli application.
 
-use riperf3::iperf_api;
+// Use clap to parse the command line arguments.
+use clap::Parser;
+
+// Use log and log4rs for logging.
+use log::LevelFilter;
+use log4rs::append::console::ConsoleAppender;
+use log4rs::config::{Appender, Config, Logger, Root};
+use log4rs::encode::pattern::PatternEncoder;
+
+// riperf3 CLI module
+mod cli;
+use cli::Cli;
 
 fn main() {
-    let test = iperf_api::IperfTest {
-        ..iperf_api::IperfTest::default()
+    // Parse the command line arguments
+    let cli = Cli::parse();
+
+    // Configure log4rs
+    let log_level = cli.debug.unwrap_or(0); // Default to 0 if not specified, which is ERROR
+    configure_log4rs(log_level);
+    log::trace!("log4rs configured with verbosity: {}", log_level);
+
+    if let Some(server_addr) = cli.client {
+        log::info!(
+            "Running in client mode, connecting to server at {} on port {}",
+            server_addr,
+            cli.port
+        );
+    } else if cli.server {
+        log::info!("Running in server mode, listening on port {}", cli.port);
+    } else {
+        log::error!("No mode specified, exiting.");
+        return;
+    }
+}
+
+// ////////////////////////////////////////////////////////////////////////////
+// Log4rs configuration
+fn configure_log4rs(verbosity: u8) {
+    let level = match verbosity {
+        0 => LevelFilter::Error,
+        1 => LevelFilter::Warn,
+        2 => LevelFilter::Info,
+        3 => LevelFilter::Debug,
+        _ => LevelFilter::Trace,
     };
 
-    println!("{:?}", test);
+    let stdout = ConsoleAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("{d} - {l} - {m}{n}")))
+        .build();
+
+    let config = Config::builder()
+        .appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .logger(Logger::builder().build("app::backend::db", LevelFilter::Info))
+        .build(Root::builder().appender("stdout").build(level))
+        .unwrap();
+
+    log4rs::init_config(config).unwrap();
 }
