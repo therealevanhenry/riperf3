f93f68104b33ca8a9c4decf0e79afcb8fea231dc
Evan Henry <evan@perfx.cloud>
2024-11-20 15:24:41 -0500

    Added the `vprintln!` macro for verbose mode support.

--- riperf3/src/lib.rs
+++ riperf3/src/lib.rs
@@ -1,5 +1,15 @@
 pub mod iperf_api;
 
+// Verbose mode println!(...) macro
+#[macro_export]
+macro_rules! vprintln {
+    ($verbose:expr, $($arg:tt)*) => {
+        if $verbose {
+            println!($($arg)*);
+        }
+    };
+}
+
 pub fn run_client() {
     println!("Running client");
 }
